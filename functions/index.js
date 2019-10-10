af318849c83b1c3827147d9129d7b8f8acaae37a
Anthony de Vellis <anthonydevellis@gmail.com>
2019-10-09 22:28:05 -0400

    tried fixing cors

--- functions/index.js
+++ functions/index.js
@@ -3,33 +3,47 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// const cors = require('cors')({origin: true});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.getUser = functions.https.onRequest((req, res) => {
-  const uid = req.query.uid;
   
-  if (uid) {
-    res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    console.log('preflight');
     res.set('Access-Control-Allow-Methods', 'GET');
     res.set('Access-Control-Allow-Headers', 'Content-Type');
-    // res.set('Access-Control-Max-Age', '3600');
-    const responseJSON = {};
-    admin.database().ref(`/users/${uid}/contactInfo`).once('value')
-      .then(snap => {
-        console.log(`In then block: ${snap.val()}`)
-        responseJSON.firstName = snap.val().firstName;
-        responseJSON.lastName = snap.val().lastName;
-        res.json(responseJSON);
-      })
-      .catch((err) => {
-        res.json({ error: "Database access failed" });
-        console.log("There was an error: " + err);
-      })
-      .finally(() => {
-        console.log(responseJSON)
-        res.end();
-      });
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else {
+    const uid = req.query.uid;
+    if (uid) {
+      // res.set('Access-Control-Max-Age', '3600');
+      const responseJSON = {};
+      admin.database().ref(`/users/${uid}/contactInfo`).once('value')
+        .then(snap => {
+          console.log(`In then block: ${snap.val()}`)
+          responseJSON.firstName = snap.val().firstName;
+          responseJSON.lastName = snap.val().lastName;
+          res.status(200).json(responseJSON);
+        })
+        .catch((err) => {
+          res.json({ error: "Database access failed" });
+          console.log("There was an error: " + err);
+        })
+        .finally(() => {
+          console.log(responseJSON)
+          res.end();
+        });
+    }
   }
 
+  // cors(req, res, () => {
+  //   const uid = req.query.uid;
+
+    
+  // });
 });
